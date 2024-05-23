6fd5b4a39a25bdd57a690e44fe0683e4eff94265
Jacob Boyce <jboyce313@gmail.com>
2024-05-23 15:45:39 -0500

    get rt score from response and parse as int

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -7,12 +7,19 @@ const Game = async () => {
     `https://www.omdbapi.com/?i=${movieID}&apikey=efb9b6cf`
   );
   const movie = await response.json();
+  const rtScore = parseInt(
+    movie.Ratings.find(
+      (rating: { Source: string }) => rating.Source === "Rotten Tomatoes"
+    )?.Value || "0"
+  );
   console.log(movie);
 
   return (
     <div>
       <h1>Guess the Score!</h1>
-      <h2>{movie.Title}</h2>
+      <h2>
+        {movie.Title} ({movie.Year})
+      </h2>
       <p>{movie.Plot}</p>
       <img src={movie.Poster} />
     </div>
